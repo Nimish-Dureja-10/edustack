fcca9637bea107d02b2554d36d8ed0ff4120a60f
Nimish-Dureja-10 <durejanimish14@gmail.com>
2023-09-25 23:50:55 +0530

    Course Detailed Page designed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import ResetPassword from './components/Auth/ResetPassword';
 import Contact from './components/Contact/Contact';
+import CoursePage from './components/CourseDetails/CoursePage';
 import Courses from './components/Courses/Courses';
 import Home from './components/Home/Home';
 import Footer from './components/Layout/Footer/Footer';
@@ -17,6 +18,9 @@ import Subscribe from './components/Payments/Subscribe';
 import Request from './components/Request/Request';
 
 function App() {
+  window.addEventListener('contextmenu',(e)=> {
+    e.preventDefault();
+  });
   return (
     <Router>
       <Header />
@@ -27,6 +31,7 @@ function App() {
           <Route path="/forgetpassword" element={<ForgetPassword />} />
           <Route path="/resetpassword/:token" element={<ResetPassword />} />
           <Route path="/courses" element={<Courses />} />
+          <Route path="/course/:id" element={<CoursePage />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/request" element={<Request />} />
           <Route path="/about" element={<About />} />
---
+++ frontend/src/components/CourseDetails/CoursePage.jsx
@@ -0,0 +1,69 @@
+import { Box, Grid, Heading, Text, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import introVideo from "../../assets/videos/introVideo.mp4";
+
+const CoursePage = () => {
+
+  const [lectureNumber,setLectureNumber] = useState(0);
+  const lectures = [
+      {
+        _id:"ddaadasd",
+        title:"Sample",
+        description:"Sample description",
+        video : {
+            url : "shdhuhuh"
+        }
+      },
+      {
+        _id:"ddaadasd2",
+        title:"Sample2",
+        description:"Sample2 description",
+        video : {
+            url : "shdhuhuh2"
+        }
+      },
+      {
+        _id:"ddaadasd3",
+        title:"Sample3",
+        description:"Sample3 description",
+        video : {
+            url : "shdhuhuh3"
+        }
+      },
+      {
+        _id:"ddaadasd4",
+        title:"Sample4",
+        description:"Sample4 description",
+        video : {
+            url : "shdhuhuh4"
+        }
+      },
+    ];
+
+  return (
+    <Grid minH='90vh' templateColumns={['1fr','3fr 1fr']}>
+        <Box>
+            <video width='100%' autoplay controls src={introVideo}
+            controlsList='nodownload noremoteplayback'
+            disablePictureInPicture
+            disableRemotePlayback
+            >
+            </video>
+            <Heading children={`#${lectureNumber+1} - ${lectures[lectureNumber].title}`} m='4' />
+            <Heading children='Description' m='4' />
+            <Text children={`${lectures[lectureNumber].description}`} m='4' />
+        </Box>
+        <VStack>
+            {lectures.map((element,index)=>(
+                <button onClick={()=>setLectureNumber(index)}  key={element._id} style={{width:"100%",padding:'1rem',textAlign:"center",margin:"0", borderBottom:"1px solid rgba(0,0,0,0.2)"}}>
+                    <Text numberOfLines={1}>
+                        #{index+1} {element.title}
+                    </Text>
+                </button>
+            ))}
+        </VStack>
+    </Grid>
+  )
+}
+
+export default CoursePage
