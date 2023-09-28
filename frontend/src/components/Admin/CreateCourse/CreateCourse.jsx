61ca442e0314fd0f2ee1c57d553bb2b13fb843ab
Nimish-Dureja-10 <durejanimish14@gmail.com>
2023-09-28 13:17:06 +0530

    Admin Dashboard User Pagge designed

--- frontend/src/components/Admin/CreateCourse/CreateCourse.jsx
+++ frontend/src/components/Admin/CreateCourse/CreateCourse.jsx
@@ -1,14 +1,60 @@
-import { Box, Grid } from '@chakra-ui/react'
-import React from 'react'
+import { Button, Container, Grid, Heading, Image, Input, Select, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import Sidebar from '../Sidebar'
 import cursor from '../../../assets/images/cursor2.png'
+import { fileUploadCss } from '../../Auth/Register'
 
 const CreateCourse = () => {
+    const [title,setTitle] = useState('');
+    const [description,setDescription] = useState('');
+    const [createdBy,setCreatedBy] = useState(''); 
+    const [category,setCategory] = useState(''); 
+    const [image,setImage] = useState(''); 
+    const [imagePrev,setImagePrev] = useState(''); 
+
+    const changeImageHandler = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+          setImagePrev(reader.result);
+          setImage(file);
+        }
+      } 
+
+    const categories = [
+        "Web Development","Artificial Intelligence","Data Science",
+        "BlockChain Development","Flutter Development",
+        "React Native","Game Development"
+    ] 
+
   return (
     <Grid css={{cursor:`url(${cursor}), default`}} minH={'100vh'} templateColumns={['1fr','5fr 1fr']}>
-        <Box>
-
-        </Box>
+        <Container py='16'>
+            <form>
+                <Heading textTransform={'uppercase'} children='Create Course' my='16' textAlign={['center','left']} />
+                <VStack m='auto' spacing={'8'}  >
+                    <Input value={title} onChange={(e)=>setTitle(e.target.value)} placeholder='Title' type={'text'} focusBorderColor='purple.300' />
+                    <Input value={description} onChange={(e)=>setDescription(e.target.value)} placeholder='Description' type={'text'} focusBorderColor='purple.300' />
+                    <Input value={createdBy} onChange={(e)=>setCreatedBy(e.target.value)} placeholder='Creator Name' type={'text'} focusBorderColor='purple.300' />
+                    <Select focusBorderColor='purple.300' value={category} onChange={(e)=>setCategory(e.target.value)} >
+                        <option value=''>
+                            Category
+                        </option>
+                        {categories.map((item)=>(
+                            <option key={item} value={item}>{item}</option>
+                        ))}
+                    </Select>
+                    <Input accept='image/*' required type={'file'} focusBorderColor='purple.300' css={{'&::file-selector-button':{...fileUploadCss,color:"purple"}}} onChange={changeImageHandler} />
+                    {imagePrev && (
+                        <Image src={imagePrev} boxSize='64' objectFit={'contain'} />
+                    )}
+                    <Button width={'full'} colorScheme={'purple'}>
+                        Create
+                    </Button>   
+                </VStack>
+            </form>
+        </Container>
         <Sidebar />
     </Grid>
   )
--- frontend/src/components/Admin/Users/Users.jsx
+++ frontend/src/components/Admin/Users/Users.jsx
@@ -1,13 +1,56 @@
-import { Box, Grid } from '@chakra-ui/react'
+import { Box, Button, Grid, Heading, HStack, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
 import React from 'react'
 import Sidebar from '../Sidebar'
 import cursor from '../../../assets/images/cursor2.png'
+import { RiDeleteBin7Fill } from 'react-icons/ri'
 
 const Users = () => {
+
+  const users = [{
+    _id:"weuyr283r2389re2ihdudg",
+    name:"Nimish",
+    role:"admin",
+    subscription:{
+      status:"active"
+    },
+    email:"nimish@gmail.com"
+  }]
+
+  const updateHandler = (userId) => {
+    console.log(userId);
+  }
+
+  const deleteButtonHandler = (userId) => {
+    console.log(userId);
+  }
+
+
   return (
     <Grid css={{cursor:`url(${cursor}), default`}} minH={'100vh'} templateColumns={['1fr','5fr 1fr']}>
-        <Box>
-
+        <Box p={['0','16']} overflowX={'auto'}>
+          <Heading textTransform={'uppercase'} children='All Users' my='16' textAlign={['center','left']} />
+          <TableContainer width={['100vw','full']}>
+            <Table variant={'simple'} size='lg'>
+              <TableCaption>
+                All Available Users In The Database
+              </TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>ID</Th>
+                  <Th>NAME</Th>
+                  <Th>EMAIL</Th>
+                  <Th>ROLE</Th>
+                  <Th>SUBSCRIPTION</Th>
+                  <Th isNumeric>ACTION</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {users.map((item)=>(
+                  <Row key={item._id} item={item} updateHandler={updateHandler} deleteButtonHandler={deleteButtonHandler}  />
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
         </Box>
         <Sidebar />
     </Grid>
@@ -15,3 +58,33 @@ const Users = () => {
 }
 
 export default Users
+
+function Row({item,updateHandler,deleteButtonHandler}) {
+  return (
+    <Tr>
+      <Td>
+        #{item._id}
+      </Td>
+      <Td>
+        {item.name}
+      </Td>
+      <Td>
+        #{item.email}
+      </Td>
+      <Td>
+        #{item.role}
+      </Td>
+      <Td>
+        #{item.subscription.status==='active'?"Active":"Not Active"}
+      </Td>
+      <Td isNumberic>
+        <HStack justifyContent={'flex-end'}>
+          <Button onClick={()=>updateHandler(item._id)} variant={'outline'} color={'purple.500'} >Change Role</Button>
+          <Button onClick={()=>deleteButtonHandler(item._id)} color={'purple.600'}>
+            <RiDeleteBin7Fill />
+          </Button>
+        </HStack>
+      </Td>
+    </Tr>
+  )
+}
